ad7b679da42f38fdc0a95dd5cb94ab8e1359166d
User <kostia71@gmail.com>
2022-02-17 22:28:33 +0200

    update ProjectController

---
+++ src/main/java/com/crazytasknewassembly/api/controllers/ProjectController.java
@@ -0,0 +1,99 @@
+package com.crazytasknewassembly.api.controllers;
+
+import com.crazytasknewassembly.api.controllers.helpers.ControllerHelper;
+import com.crazytasknewassembly.api.dto.AckDto;
+import com.crazytasknewassembly.api.dto.ProjectDto;
+import com.crazytasknewassembly.api.exceptions.BadRequestException;
+import com.crazytasknewassembly.api.factories.ProjectDtoFactory;
+import com.crazytasknewassembly.store.entities.ProjectEntity;
+import com.crazytasknewassembly.store.repositories.ProjectRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Transactional
+@RestController
+public class ProjectController {
+
+    ProjectRepository projectRepository;
+
+    ProjectDtoFactory projectDtoFactory;
+
+    ControllerHelper controllerHelper;
+
+    public static final String FETCH_PROJECTS = "/api/projects";
+    public static final String CREATE_OR_UPDATE_PROJECT = "/api/projects";
+    public static final String DELETE_PROJECT = "/api/projects/{project_id}";
+
+    @GetMapping(FETCH_PROJECTS)
+    public List<ProjectDto> fetchProjects(
+            @RequestParam(value = "prefix_name", required = false) Optional<String> optionalPrefixName) {
+
+        optionalPrefixName = optionalPrefixName.filter(prefixName -> !prefixName.trim().isEmpty());
+
+        Stream<ProjectEntity> projectStream = optionalPrefixName
+                .map(projectRepository::streamAllByNameStartsWithIgnoreCase)
+                .orElseGet(projectRepository::streamAllBy);
+
+        return projectStream
+                .map(projectDtoFactory::makeProjectDto)
+                .collect(Collectors.toList());
+    }
+
+    @PutMapping(CREATE_OR_UPDATE_PROJECT)
+    public ProjectDto createOrUpdateProject(
+            @RequestParam(value = "project_id", required = false) Optional<Long> optionalProjectId,
+            @RequestParam(value = "project_name", required = false) Optional<String> optionalProjectName) {
+
+        optionalProjectName = optionalProjectName.filter(projectName -> !projectName.trim().isEmpty());
+
+        boolean isCreate = !optionalProjectId.isPresent();
+
+        if (isCreate && !optionalProjectName.isPresent()) {
+            throw new BadRequestException("Project name can't be empty.");
+        }
+
+        final ProjectEntity project = optionalProjectId
+                .map(controllerHelper::getProjectOrThrowException)
+                .orElseGet(() -> ProjectEntity.builder().build());
+
+        optionalProjectName
+                .ifPresent(projectName -> {
+
+                    projectRepository
+                            .findByName(projectName)
+                            .filter(anotherProject -> !Objects.equals(anotherProject.getId(), project.getId()))
+                            .ifPresent(anotherProject -> {
+                                throw new BadRequestException(
+                                        String.format("Project \"%s\" already exists.", projectName)
+                                );
+                            });
+
+                    project.setName(projectName);
+                });
+
+        final ProjectEntity savedProject = projectRepository.saveAndFlush(project);
+
+        return projectDtoFactory.makeProjectDto(savedProject);
+    }
+
+    @DeleteMapping(DELETE_PROJECT)
+    public AckDto deleteProject(@PathVariable("project_id") Long projectId) {
+
+        controllerHelper.getProjectOrThrowException(projectId);
+
+        projectRepository.deleteById(projectId);
+
+        return AckDto.makeDefault(true);
+    }
+}
