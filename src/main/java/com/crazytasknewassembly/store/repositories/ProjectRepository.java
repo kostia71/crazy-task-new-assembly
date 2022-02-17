882693c3c8b843247001f32e8701ad4ee2b50a7a
User <kostia71@gmail.com>
2022-02-17 22:29:35 +0200

    update ProjectRepository

---
+++ src/main/java/com/crazytasknewassembly/store/repositories/ProjectRepository.java
@@ -0,0 +1,18 @@
+package com.crazytasknewassembly.store.repositories;
+
+import com.crazytasknewassembly.store.entities.ProjectEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+
+    Optional<ProjectEntity> findByName(String name);
+
+    Stream<ProjectEntity> streamAllBy();
+
+    Stream<ProjectEntity> streamAllByNameStartsWithIgnoreCase(String prefixName);
+}
