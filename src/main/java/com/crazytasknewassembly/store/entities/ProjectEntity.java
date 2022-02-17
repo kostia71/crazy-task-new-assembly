249129720c158fce4f6a9a94c6f7485342519c4f
User <kostia71@gmail.com>
2022-02-17 22:29:21 +0200

    update ProjectDtoEntity

---
+++ src/main/java/com/crazytasknewassembly/store/entities/ProjectEntity.java
@@ -0,0 +1,48 @@
+package com.crazytasknewassembly.store.entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "project")
+public class ProjectEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(unique = true)
+    String name;
+
+    @Builder.Default
+    Instant updatedAt = Instant.now();
+
+    @Builder.Default
+    Instant createdAt = Instant.now();
+
+    @Builder.Default
+    @OneToMany
+    @JoinColumn(name = "project_id")
+    List<TaskStateEntity> taskStates = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "ProjectEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", updatedAt=" + updatedAt +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
