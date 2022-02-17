2a83932950dda039cfbfd4c478404d0c5dc7f72a
User <kostia71@gmail.com>
2022-02-17 22:29:04 +0200

    update ProjectDtoFactory

---
+++ src/main/java/com/crazytasknewassembly/api/factories/ProjectDtoFactory.java
@@ -0,0 +1,28 @@
+package com.crazytasknewassembly.api.factories;
+
+import com.crazytasknewassembly.api.dto.ProjectDto;
+import com.crazytasknewassembly.store.entities.ProjectEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectDtoFactory {
+
+    public ProjectDto makeProjectDto(ProjectEntity entity) {
+
+        return ProjectDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .build();
+    }
+
+    public ProjectEntity makeProjectEntity(ProjectDto dto){
+        return ProjectEntity.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .createdAt(dto.getCreatedAt())
+                .updatedAt(dto.getUpdatedAt())
+                .build();
+    }
+}
