801110d3c077d647afe5a960269d7ce104b416e6
User <kostia71@gmail.com>
2022-02-17 22:28:00 +0200

    update InitiateUtils

---
+++ src/main/java/com/crazytasknewassembly/utils/InitiateUtils.java
@@ -0,0 +1,35 @@
+package com.crazytasknewassembly.utils;
+
+import com.crazytasknewassembly.api.dto.ProjectDto;
+import com.crazytasknewassembly.service.ProjectServiceImpl;
+import com.crazytasknewassembly.store.repositories.ProjectRepository;
+import com.crazytasknewassembly.store.entities.ProjectEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+
+@Service
+@RequiredArgsConstructor
+public class InitiateUtils implements CommandLineRunner {
+
+    private final ProjectServiceImpl projectService;
+
+    @Override
+    public void run(String... args) {
+
+        ProjectDto projectdto1 = new ProjectDto();
+        projectdto1.setName("one");
+        ProjectDto projectdto2 = new ProjectDto();
+        projectdto2.setName("two");
+
+        projectService.save(projectdto1);
+        projectService.save(projectdto2);
+
+        for (ProjectDto p : projectService.findAll()) {
+            System.out.println(p);
+        }
+    }
+}
